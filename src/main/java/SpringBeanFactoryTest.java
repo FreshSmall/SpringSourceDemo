3cbe7428241d9eb9c56e64f035907431f88fc7c8
yinchao <291355504@qq.com>
2019-05-16 20:49:58 +0800

    源码学习第一天

---
+++ src/main/java/SpringBeanFactoryTest.java
@@ -0,0 +1,24 @@
+import cn.com.bean.Car;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.reflect.Type;
+
+public class SpringBeanFactoryTest {
+
+	private static final Gson gson = new GsonBuilder().serializeNulls().create();
+
+	private static final Gson gson1 = new Gson();
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:beans.xml");
+		Car car = (Car) ctx.getBean("car");
+		System.out.println(car.getName());
+		System.out.println(car.getPrice());
+		/*Car car = new Car();
+		System.out.println(gson.toJson(car));
+		System.out.println(gson1.toJson(null));*/
+	}
+}
